f6b7db859e633340a878d7b5906e691b651cb961
Harsh Adarsh <108008827+harshadarsh2001@users.noreply.github.com>
2024-01-29 11:54:58 +0530

    Update RabbitMQConfig.java

--- src/main/java/com/harshproject/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/harshproject/rabbitmq/RabbitMQConfig.java
@@ -1,34 +1,40 @@
 package com.harshproject.rabbitmq;
+
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
-public class RabbitMQConfig{
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+    @Value("${rabbitmq.queue.durable}")
+    private boolean isQueueDurable;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.binding.routingKey}")
+    private String routingKey;
 
     @Bean
     Queue queue() {
-        return new Queue("department-queue", true);
-        
+        return new Queue(queueName, isQueueDurable);
     }
-	
-//	@Bean
-//    public Queue queue() {
-//        return QueueBuilder.durable("department-queue")
-//            .withArgument("x-message-ttl", 40000) // Set TTL to 5000 milliseconds (5 seconds)
-//            .build();
-//    }
+
     @Bean
     DirectExchange directExchange() {
-        return new DirectExchange("department-exchange");
-
+        return new DirectExchange(exchangeName);
     }
+
     @Bean
     Binding binding(Queue queue, DirectExchange directExchange) {
-        return BindingBuilder.bind(queue).to(directExchange).with("myRoutingKey");
+        return BindingBuilder.bind(queue).to(directExchange).with(routingKey);
     }
 }
-
